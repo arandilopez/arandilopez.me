541d6f01056f3ff0ec8cb6a2795a0afdfba742d3
Arandi Lopez <arandilopez.93@gmail.com>
2018-10-27 10:07:13 -0500

    Fix jquery document ready

--- source/javascripts/site.js
+++ source/javascripts/site.js
@@ -1,30 +1,32 @@
-window.Popper = require('popper.js').default
 try {
-    window.$ = window.jQuery = require('jquery')
-    require('jquery.easing')
-    require('bootstrap')
-} catch (e) {}
+  window.Popper = require('popper.js').default
+  window.$ = window.jQuery = require('jquery')
+  require('jquery.easing')
+  require('bootstrap')
+} catch (e) { console.error(e); }
 
-// Smooth scrolling using jQuery easing
-$('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
-  if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-    var target = $(this.hash)
-    target = target.length ? target : $('[name=' + this.hash.slice(1) + ']')
-    if (target.length) {
-      $('html, body').animate({
-        scrollTop: (target.offset().top)
-      }, 1000, "easeInOutExpo")
-      return false
+$(document).ready(function () {
+  // Smooth scrolling using jQuery easing
+  $('a.js-scroll-trigger[href*="#"]:not([href="#"])').click(function () {
+    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+      var target = $(this.hash)
+      target = target.length ? target : $('[name=' + this.hash.slice(1) + ']')
+      if (target.length) {
+        $('html, body').animate({
+          scrollTop: (target.offset().top)
+        }, 1000, "easeInOutExpo")
+        return false
+      }
     }
-  }
-})
+  })
 
-// Closes responsive menu when a scroll trigger link is clicked
-$('.js-scroll-trigger').click(() => {
-  $('.navbar-collapse').collapse('hide')
-})
+  // Closes responsive menu when a scroll trigger link is clicked
+  $('.js-scroll-trigger').click(() => {
+    $('.navbar-collapse').collapse('hide')
+  })
 
-// Activate scrollspy to add active class to navbar items on scroll
-$('body').scrollspy({
-  target: '#sideNav'
+  // Activate scrollspy to add active class to navbar items on scroll
+  $('body').scrollspy({
+    target: '#sideNav'
+  })
 })
