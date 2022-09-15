1b501d5826973ea8d0bdad6b73ea89726ec5f017
Arandi Lopez <arandilopez.93@gmail.com>
2022-09-14 20:11:30 -0500

    chore: update to tailwindcss v3

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,23 +1,27 @@
 module.exports = {
-  purge: ['./src/**/*.html', './src/**/*.liquid', './src/**/*.md', './frontend/**/*.js'],
-  darkMode: 'media', // or 'media' or 'class'
+  content: [
+    "./src/**/*.html",
+    "./src/**/*.liquid",
+    "./src/**/*.md",
+    "./frontend/**/*.js",
+  ],
   theme: {
     extend: {
       colors: {
         accent: {
-          DEFAULT: '#e91d63',
-          dark: '#bf124d'
-        }
+          DEFAULT: "#e91d63",
+          dark: "#bf124d",
+        },
       },
       fontFamily: {
         sans: ['"Open Sans"'],
-        title: ['Oswald'],
-        mono: ['"Fira Mono"']
-      }
+        title: ["Oswald"],
+        mono: ['"Fira Mono"'],
+      },
     },
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
